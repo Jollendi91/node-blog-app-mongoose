14bbb392bbd683ed31a76fceb33407b8ed14e227
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-04-21 08:26:43 -0500

    add GET to posts

--- models.js
+++ models.js
@@ -5,9 +5,23 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
     title: {type: String, required: true},
     content: {type: String, required: true},
-    author: {type: String, required: true},
+    author: {type: Object, required: true},
     created: {type: Date, default: Date.now()}
 });
+
+postSchema.virtual('authorString').get(function() {
+    return `${this.author.firstName} ${this.author.lastName}`.trim()
+});
+
+postSchema.methods.serialize = function() {
+    return {
+        id: this._id,
+        title: this.title,
+        author: this.authorString,
+        content: this.content,
+        created: this.created
+    };
+}
 
 const Posts = mongoose.model('Posts', postSchema);
 
--- postsRoutes.js
+++ postsRoutes.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
 const { Posts } = require('./models');
 
 router.get('/', (req, res) =>{
     Posts
         .find()
         .then(posts => {
-            res.json({posts});
+            res.json({
+                posts: posts.map((post) => post.serialize())
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({message: 'Internal server error'});
+        });
+});
+
+router.get('/:id', (req, res) => {
+    Posts
+        .findById(req.params.id)
+        .then(post => res.json(post.serialize()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({message: 'Internal server error'});
         });
 });
 
